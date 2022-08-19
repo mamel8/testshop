e5ab1d93c422bf56280d179dd335c82733a2cb80
Hapanenka Andrei <G5119@ya.ru>
2022-08-19 10:21:33 +0300

    +dto

---
+++ src/main/java/by/andrey/springcorse/ShopApp/dto/PersonDTO.java
@@ -0,0 +1,52 @@
+package by.andrey.springcorse.ShopApp.dto;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+public class PersonDTO {                //слоя между сущностью и контроллером
+
+    @Size(min = 3, max = 25)
+    private String name;
+
+    @Size(min = 3, max = 50)
+    private String email;
+
+    @Size(min = 3, max = 25)
+    private String phone;
+
+    private LocalDateTime registration;
+
+    public LocalDateTime getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(LocalDateTime registration) {
+        this.registration = registration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+}
